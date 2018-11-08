5d45c7c61bad7d5e0c585b828d0d674650432373
lk <lkghost7@gmail.com>
2018-11-08 15:08:09 +0300

    комиты после форс пуша

--- database/src/main/java/by/itacademy/dao/generic/CompanDao.java
+++ database/src/main/java/by/itacademy/dao/generic/CompanDao.java
@@ -1,14 +1,8 @@
 package by.itacademy.dao.generic;
 
-
-
-//public interface CompanDao extends BaseDao<Long, Company> {
-//    Company findByFirstName (String firstName);
-//}
-
+import by.itacademy.dao.generic.BaseDao;
 import by.itacademy.model.Company;
 
-public interface EmployeeDao extends BaseDao<Long, Company> {
-
+public interface CompanDao extends BaseDao<Company> {
 
 }
